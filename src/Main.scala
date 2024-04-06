b7dc1bd8ccc2222d58663b6a23cb21f0e095b30c
Manuel Almeida <66184545+manu183@users.noreply.github.com>
2024-04-06 17:02:53 +0100

    T4 done

--- src/Main.scala
+++ src/Main.scala
@@ -1,4 +1,4 @@
-import Utils.{Board}
+import Utils.{Board, Coord2D}
 object Main {
   def main(args: Array[String]): Unit = {
     //println("Projeto PPM!")
@@ -7,8 +7,21 @@ object Main {
     //println(randomChar(teste._2)._1)
     val board: Utils.Board = List(List('a', 'b', 'c'), List('d', 'e', 'f'), List('g', 'h', 'i'))
 
-    val updatedBoard = Tasks.fillOneCell(board,'x', (1,1))
+    val updatedBoard = Tasks.fillOneCell(board,'x', (1,0))
+    val updated2 = Tasks.fillWord(board, "tes", List((0,0),(0,1),(0,2)))
+
+
+    val updated3 = Tasks.setBoardsWithWords(board, List("Tes","Rei","Til"), List(
+      List[Coord2D]((0,0),(0,1),(0,2)),
+      List[Coord2D]((1,0),(1,1),(1,2)),
+      List[Coord2D]((2,0),(2,1),(2,2))))
     Utils.printBoard(updatedBoard)
+    println()
+    Utils.printBoard(updated2)
+    println()
+    Utils.printBoard(updated3)
+    println("-"*32)
+
   }
 
 }
--- src/Tasks.scala
+++ src/Tasks.scala
@@ -1,6 +1,8 @@
 
 import Utils.{Board, Coord2D}
 
+import scala.annotation.tailrec
+
 object Tasks {
   //t1
   def randomChar(rand: MyRandom): (Char, MyRandom) = {
@@ -30,29 +32,52 @@ object Tasks {
     //board.updated(coord._1, updatedRow)
   }
 
+  //t3
+  @tailrec
+  def setBoardsWithWords(board: Board, words: List[String], positions: List[List[Coord2D]]): Board = {
+    @tailrec
+    def fillWord(board: Board, word: String, positions: List[Coord2D]): Board = {
+      positions match {
+        case Nil => board
+        case _ => fillWord(fillOneCell(board, word.head, positions.head), word.tail, positions.tail)
+      }
+    }
+    positions match {
+      case Nil => board
+      case _  => setBoardsWithWords(fillWord(board, words.head, positions.head), words.tail, positions.tail)
+    }
+  }
+
+
   //t4
-  def completeBoardRandomly(board:Board, r:MyRandom, f: MyRandom => (Char, MyRandom)):(Board, MyRandom)={
-    def fillLine(line:List[Char], r:MyRandom):(List[Char], MyRandom) =
-    {
+  def completeBoardRandomly(board: Board, r: MyRandom, f: MyRandom => (Char, MyRandom)): (Board, MyRandom) = {
+    def fillLine(line: List[Char], r: MyRandom): (List[Char], MyRandom) = {
       line match {
-        case Nil=>(Nil,r)
-        case x::xs=>
-          if (x>='a'&&x<='z') { val aux=fillLine(xs,r)
-            (x::aux._1,aux._2)
+        case Nil => (Nil, r)
+        case x :: xs =>
+          if (x >= 'a' && x <= 'z') {
+            val aux = fillLine(xs, r)
+            (x :: aux._1, aux._2)
           }
-          else {  val aux=fillLine(xs,f(r)._2)
-            (f(r)._1::aux._1,aux._2)
+          else {
+            val aux = fillLine(xs, f(r)._2)
+            (f(r)._1 :: aux._1, aux._2)
           }
       }
 
     }
-    def fillBoard(board:Board,r: MyRandom):(Board,MyRandom)={board match {
-      case Nil=>(Nil,r)
-      case x::xs=>{ val aux=fillLine(x,r)
-        val aux2=fillBoard(xs,aux._2)
-        (List(aux._1) ++ aux2._1,aux._2)}
-    }
+
+    def fillBoard(board: Board, r: MyRandom): (Board, MyRandom) = {
+      board match {
+        case Nil => (Nil, r)
+        case x :: xs => {
+          val aux = fillLine(x, r)
+          val aux2 = fillBoard(xs, aux._2)
+          (List(aux._1) ++ aux2._1, aux._2)
+        }
+      }
     }
     fillBoard(board, r)
   }
+
 }
