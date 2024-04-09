79c435bfdc0a9f9614eb2a4dcfd39c029c43ac9f
pinhas-manuel <150682413+pinhas-manuel@users.noreply.github.com>
2024-04-09 16:16:46 +0100

    kek

--- src/Main.scala
+++ src/Main.scala
@@ -1,16 +1,23 @@
-import Utils.{Board, Direction}
+import Utils.{Board, Direction, jogarMenu, showTakeChoice}
+
+import scala.sys.exit
 object Main {
   def main(args: Array[String]): Unit = {
-    //println("Projeto PPM!")
-    //val teste =Tasks.randomChar(MyRandom(10))
-    //println(teste._1)
-    //println(randomChar(teste._2)._1)
-    val board: Utils.Board = List(List('s', 'b', 'c'), List('d', 'l', 'f'), List('b', 'h', 'i'))
-    print(Tasks.play(board,"slb",(0,0),Direction.SouthWest))
-    //val board1= Tasks.fillOneCell(board,'l',(2,0))
-    //Utils.printBoard1(board1)
-    //val updatedBoard = Tasks.fillOneCell(board,'x', (1,0))
-    //Utils.printBoard(updatedBoard)
+
+    showTakeChoice()
+    val (r,tamanho,palavras,coordenadas) = Utils.readFromFile("teste.txt")
+    val board= Tasks.setBoardsWithWords(List.fill(tamanho, tamanho)(0),palavras,coordenadas)
+    val board1=Tasks.completeBoardRandomly(board,MyRandom(r),Tasks.randomChar)
+
+  }
+  def jogar(board: Board,procura:List[String],encontradas:List[String]):Unit={
+    if (procura.isEmpty) exit(1)
+    Utils.jogarMenu() match  {
+      case 0 =>exit(1)
+      case 1=>Utils.getPlay
+      case 2=> main(Array())
+    }
+
   }
 
 }
--- src/Utils.scala
+++ src/Utils.scala
@@ -1,3 +1,6 @@
+import Utils.Coord2D
+
+import scala.sys.exit
 
 object Utils {
 
@@ -8,42 +11,65 @@ object Utils {
     type Direction = Value
     val North, South, East, West,
     NorthEast, NorthWest, SouthEast, SouthWest = Value
-    def addDirection(coord:Coord2D,dir:Direction):Coord2D={
+
+    def addDirection(coord: Coord2D, dir: Direction): Coord2D = {
       dir match {
-        case North=>(coord._1-1,coord._2)
-        case South=>(coord._1+1,coord._2)
-        case East=> (coord._1,coord._2-1)
-        case West=>(coord._1,coord._2+1)
-        case NorthEast=>(coord._1-1,coord._2-1)
-        case NorthWest=>(coord._1-1,coord._2+1)
-        case SouthEast=>(coord._1+1,coord._2-1)
-        case SouthWest=>(coord._1+1,coord._2+1)
+        case North => (coord._1 - 1, coord._2)
+        case South => (coord._1 + 1, coord._2)
+        case East => (coord._1, coord._2 - 1)
+        case West => (coord._1, coord._2 + 1)
+        case NorthEast => (coord._1 - 1, coord._2 - 1)
+        case NorthWest => (coord._1 - 1, coord._2 + 1)
+        case SouthEast => (coord._1 + 1, coord._2 - 1)
+        case SouthWest => (coord._1 + 1, coord._2 + 1)
       }
     }
   }
 
+  def printBoard(board: Board): Unit = {
+    board.map(x => println(x.mkString("  ")))
+  }
 
-  def printBoard(board: Board) = {
-    def printRow[E](row: List[E]): Unit = {
-      row match {
-        case Nil => println()
-        case head :: tail => {
-          print(head + "  ")
-          printRow(tail)
-        }
-      }
+  def showTakeChoice(): Unit = {
+    println("1:Iniciar Tabuleiro")
+    println("2:Alterar cor das letras")
+    println("3:Sair")
+    scala.io.StdIn.readInt() match {
+      case 1 =>
+      case 2 =>
+        colorMenu()
+        showTakeChoice()
+      case 3 => exit(1)
+      case _ => showTakeChoice()
     }
 
-    def printBoardRows(board: Board): Unit = board match {
-      case Nil =>
-      case row :: tail =>
-        printRow(row)
-        printBoardRows(tail)
+  }
+  def jogarMenu():Int={
+    println("1-Inserir Palavra e direcao, formato-PALAVRA (y,x)")
+    println("2-Reiniciar")
+    println("0-Sair")
+    scala.io.StdIn.readInt() match {
+      case 1=>1
+      case 2=>2
+      case 0=>0
+      case 3=>3
     }
+  }
+  def colorMenu(): Unit = {
+    println("1:Vermelho")
+    println("2:Azul")
+    println("3:Verde")
+    println("0:Sair")
+    scala.io.StdIn.readInt() match {
+      case 1 => print("\u001B[31m")
+      case 2 => print("\u001B[34m")
+      case 3 => print("\u001B[32m")
+      case 0 =>
+      case _ => colorMenu()
+    }
+  }
+  def getPlay():(String,Coord2D)={
 
-    printBoardRows(board)
   }
-  def printBoard1(board:Board):Unit={
-    board.map(x=>println(x.mkString("  ")))
-  }
+
 }
